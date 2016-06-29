80c4c2c50763a859d2cce2cdb0521193839f9a70
AliceOonuma <oonuma@genuine-pt.jp>
2016-06-29 16:05:07 +0900

    ログイン画面を作成。

--- carmanager-web/src/main/java/jp/genuine/training/carmanager/web/IndexController.java
+++ carmanager-web/src/main/java/jp/genuine/training/carmanager/web/IndexController.java
@@ -1,19 +1,36 @@
 package jp.genuine.training.carmanager.web;
 
-import javax.servlet.http.HttpServletRequest;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+
+import jp.genuine.training.carmanager.model.car.summary.CarSummaryList;
+import jp.genuine.training.carmanager.model.user.LoginUser;
+import jp.genuine.training.carmanager.service.car.summary.CarSummaryListService;
 
 @Controller
 @RequestMapping("/index")
 public class IndexController {
 
-	@RequestMapping(value="", method=RequestMethod.GET)
-    public String show(Model model, HttpServletRequest request){
-		return "/index/index";
+	@Autowired
+	private CarSummaryListService carSummaryListService;
+
+	@RequestMapping(value="",method=RequestMethod.GET)
+	public String list(
+			@AuthenticationPrincipal LoginUser loginUser,
+			Model model,
+			WebRequest request)
+	{
+
+		CarSummaryList carSummaryList = carSummaryListService.listOf(loginUser);
+
+		model.addAttribute("carSummaryList",carSummaryList);
+
+		return "/car/summary/list";
 	}
 
 }
